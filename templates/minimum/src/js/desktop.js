52cc7a28583eb3bb603a7b1ba176e2f33dbcb549
Toru Kobayashi <koba0004@gmail.com>
2018-06-06 14:27:16 +0900

    feat(template): use settings for the plugin

--- templates/minimum/src/js/desktop.js
+++ templates/minimum/src/js/desktop.js
@@ -3,12 +3,34 @@ jQuery.noConflict();
 (function($, PLUGIN_ID) {
     'use strict';
 
+    function getSettingsUrl() {
+        return '/k/admin/app/' + kintone.app.getId() + '/plugin/config?pluginId=' + PLUGIN_ID;
+    }
+
     kintone.events.on('app.record.index.show', function() {
+        var config = kintone.plugin.app.getConfig(PLUGIN_ID);
+
         var spaceElement = kintone.app.getHeaderSpaceElement();
-        var el = document.createElement('div');
-        el.classList.add('plugin-space-element');
-        el.textContent = 'Hello from kintone plugin! ID:' + PLUGIN_ID;
-        spaceElement.appendChild(el);
+        var fragment = document.createDocumentFragment();
+        var headingEl = document.createElement('h3');
+        var messageEl;
+        if (config.message) {
+            messageEl = document.createElement('p');
+            messageEl.classList.add('plugin-space-message');
+            messageEl.textContent = config.message;
+        } else {
+            messageEl = document.createElement('a');
+            messageEl.classList.add('plugin-space-message');
+            messageEl.setAttribute('href', getSettingsUrl());
+            messageEl.textContent = 'Please input a message at the plugin settings!';
+        }
+
+        headingEl.classList.add('plugin-space-heading');
+        headingEl.textContent = 'Hello kintone plugin!';
+
+        fragment.appendChild(headingEl);
+        fragment.appendChild(messageEl);
+        spaceElement.appendChild(fragment);
     });
 
 })(jQuery, kintone.$PLUGIN_ID);
